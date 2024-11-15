f8d4cae98a553814c27f068c9b8b4e96c6159ea2
alex <alexesteveshq@gmail.com>
2024-11-15 23:07:28 +0100

    [Alex] V17 deploy

---
+++ icd_manager/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Medical Pathologies ICD10',
+    'summary': 'Allows to browse all available diseases from ICD10 and their codes.'
+               ' (ICD10 | International Classification of Diseases | Medical Pathology | ICD)',
+    'description': 'Allows to browse all available diseases from ICD10 and their codes.',
+    'version': '17.0.1.0',
+    'category': 'Medical',
+    'author': 'Visionee',
+    'license': 'OPL-1',
+    'website': 'https://visionee.net',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/medical_disease_views.xml',
+        'security/ir.model.access.csv'
+    ],
+    'price': 60,
+    'currency': "EUR",
+    'license': 'AGPL-3',
+    'post_init_hook': 'post_init_hook',
+    'installable': True,
+    'auto_install': False,
+}
